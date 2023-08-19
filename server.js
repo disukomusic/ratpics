69c2f079fcb9c5916d0a8fbb10c95eec70d9185d
Disuko <disukomusic@gmail.com>
2023-08-18 20:50:56 -0400

    RAT!!!!!!!

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const userDisplayNames = {};
 const userList = [];
 
 io.on('connection', (socket) => {
+	console.log('connected rat!!11', socket.id);
     socket.on('setDisplayName', (displayName) => {
         userDisplayNames[socket.id] = displayName;
         updateUserList();
@@ -28,12 +29,14 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         delete userDisplayNames[socket.id];
         updateUserList();
+		console.log('rat is dead he died the', socket.id);
         io.emit('userDisconnected', socket.id);
     });
 
     socket.on('chatMessage', (message) => {
         const displayName = userDisplayNames[socket.id] || 'Anonymous';
         const chatMessage = { user: displayName, message };
+		console.log(chatMessage)
         io.emit('chatMessage', chatMessage);
     });
 });
